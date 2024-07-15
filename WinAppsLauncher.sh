6780039e4a05d1606256779de0f1dd55473bb178
Rohan Barar <rohan.barar@gmail.com>
2024-07-15 10:44:02 +1000

    Various changes to resolve ShellCheck warnings.

--- WinAppsLauncher.sh
+++ WinAppsLauncher.sh
@@ -2,39 +2,39 @@
 
 ### GLOBAL CONSTANTS ###
 # ANSI Escape Sequences
-export readonly ERROR_TEXT="\033[1;31m"
-export readonly DEBUG_TEXT="\033[1;33m"
-export readonly STATUS_TEXT="\033[1;32m"
-export readonly ADDRESS_TEXT="\033[1;34m"
-export readonly COMMAND_TEXT="\033[0;37m"
-export readonly PATH_TEXT="\033[1;35m"
-export readonly RESET_TEXT="\033[0m"
+declare -rx ERROR_TEXT="\033[1;31m"
+declare -rx DEBUG_TEXT="\033[1;33m"
+declare -rx STATUS_TEXT="\033[1;32m"
+declare -rx ADDRESS_TEXT="\033[1;34m"
+declare -rx COMMAND_TEXT="\033[0;37m"
+declare -rx PATH_TEXT="\033[1;35m"
+declare -rx RESET_TEXT="\033[0m"
 
 # Paths
-export readonly ICONS_PATH="./Icons"
+declare -rx ICONS_PATH="./Icons"
 
 # Menu Entries
-export readonly MENU_APPLICATIONS="Applications!bash -c app_select!${ICONS_PATH}/Applications.svg"
-export readonly MENU_FORCEOFF="Force Power Off!bash -c force_power_off_vm!${ICONS_PATH}/ForceOff.svg"
-export readonly MENU_KILL="Kill FreeRDP!bash -c kill_xfreerdp!${ICONS_PATH}/Kill.svg"
-export readonly MENU_PAUSE="Pause!bash -c pause_vm!${ICONS_PATH}/Pause.svg"
-export readonly MENU_POWEROFF="Power Off!bash -c power_off_vm!${ICONS_PATH}/Power.svg"
-export readonly MENU_POWERON="Power On!bash -c power_on_vm!${ICONS_PATH}/Power.svg"
-export readonly MENU_QUIT="Quit!quit!${ICONS_PATH}/Quit.svg"
-export readonly MENU_REBOOT="Reboot!bash -c reboot_vm!${ICONS_PATH}/Reboot.svg"
-export readonly MENU_REDMOND="Windows!bash -c launch_windows!${ICONS_PATH}/Redmond.svg"
-export readonly MENU_REFRESH="Refresh Menu!bash -c refresh_menu!${ICONS_PATH}/Refresh.svg"
-export readonly MENU_RESET="Reset!bash -c reset_vm!${ICONS_PATH}/Reset.svg"
-export readonly MENU_RESUME="Resume!bash -c resume_vm!${ICONS_PATH}/Resume.svg"
-export readonly MENU_HIBERNATE="Hibernate!bash -c hibernate_vm!${ICONS_PATH}/Hibernate.svg"
+declare -rx MENU_APPLICATIONS="Applications!bash -c app_select!${ICONS_PATH}/Applications.svg"
+declare -rx MENU_FORCEOFF="Force Power Off!bash -c force_power_off_vm!${ICONS_PATH}/ForceOff.svg"
+declare -rx MENU_KILL="Kill FreeRDP!bash -c kill_xfreerdp!${ICONS_PATH}/Kill.svg"
+declare -rx MENU_PAUSE="Pause!bash -c pause_vm!${ICONS_PATH}/Pause.svg"
+declare -rx MENU_POWEROFF="Power Off!bash -c power_off_vm!${ICONS_PATH}/Power.svg"
+declare -rx MENU_POWERON="Power On!bash -c power_on_vm!${ICONS_PATH}/Power.svg"
+declare -rx MENU_QUIT="Quit!quit!${ICONS_PATH}/Quit.svg"
+declare -rx MENU_REBOOT="Reboot!bash -c reboot_vm!${ICONS_PATH}/Reboot.svg"
+declare -rx MENU_REDMOND="Windows!bash -c launch_windows!${ICONS_PATH}/Redmond.svg"
+declare -rx MENU_REFRESH="Refresh Menu!bash -c refresh_menu!${ICONS_PATH}/Refresh.svg"
+declare -rx MENU_RESET="Reset!bash -c reset_vm!${ICONS_PATH}/Reset.svg"
+declare -rx MENU_RESUME="Resume!bash -c resume_vm!${ICONS_PATH}/Resume.svg"
+declare -rx MENU_HIBERNATE="Hibernate!bash -c hibernate_vm!${ICONS_PATH}/Hibernate.svg"
 
 # Other
-export readonly VM_NAME="RDPWindows"
-export readonly RDP_COMMAND="xfreerdp"
-export readonly SLEEP_DURATION="1.5"
+declare -rx VM_NAME="RDPWindows"
+declare -rx RDP_COMMAND="xfreerdp"
+declare -rx SLEEP_DURATION="1.5"
 
 ### GLOBAL VARIABLES ###
-export WINAPPS_PATH=""    # Generated programmatically following dependency checks.
+declare -x WINAPPS_PATH=""    # Generated programmatically following dependency checks.
 
 ### WORKING DIRECTORY ###
 if cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"; then
@@ -46,9 +46,10 @@ else
 fi
 
 ### FIFO FILE & FILE DESCRIPTOR ###
-export PIPE=$(mktemp -u --tmpdir ${0##*/}.XXXXXXXX)
-mkfifo $PIPE
-exec 3<> $PIPE
+# shellcheck disable=SC2155 # Silence warning regarding declaring and assigning variables separately.
+export PIPE=$(mktemp -u --tmpdir "${0##*/}".XXXXXXXX)
+mkfifo "$PIPE"
+exec 3<> "$PIPE"
 
 ### FUNCTIONS ###
 # Process Shutdown Handler
@@ -58,7 +59,7 @@ function on_exit() {
 
     # Clean Exit
     echo "quit" >&3
-    rm -f $PIPE
+    rm -f "$PIPE"
 }
 trap on_exit EXIT
 
@@ -68,19 +69,20 @@ kill_xfreerdp() {
     echo -e "${DEBUG_TEXT}> KILL FREERDP PROCESSES${RESET_TEXT}"
 
     # Find all FreeRDP processes
-    local pids=$(pgrep $RDP_COMMAND)
+    # shellcheck disable=SC2155 # Silence warning regarding declaring and assigning variables separately.
+    local PIDS=$(pgrep "$RDP_COMMAND")
 
     # Check if any processes were found
-    if [ -n "$pids" ]; then
-        echo -e "${COMMAND_TEXT}% $(echo "$pids" | xargs kill -9)${RESET_TEXT}"
-        show_error_message "<u>KILLED</u> FreeRDP (${RDP_COMMAND}) process(es): ${pids}."
+    if [ -n "$PIDS" ]; then
+        echo -e "${COMMAND_TEXT}% $(echo "$PIDS" | xargs kill -9)${RESET_TEXT}"
+        show_error_message "<u>KILLED</u> FreeRDP (${RDP_COMMAND}) process(es): ${PIDS}."
     fi
 }
 export -f kill_xfreerdp
 
 # Error Message
 show_error_message() {
-    local message="${1}"
+    local MESSAGE="${1}"
 
     yad --error \
         --fixed \
@@ -91,7 +93,7 @@ show_error_message() {
         --selectable-labels \
         --title="WinApps Launcher" \
         --image=dialog-error \
-        --text="$message" \
+        --text="$MESSAGE" \
         --button=yad-ok:0 \
         --timeout=10 \
         --timeout-indicator=bottom &
@@ -101,14 +103,23 @@ export -f show_error_message
 # Application Selection
 app_select() {
     if check_reachable; then
+        local ALL_FILES=()
         local APP_LIST=()
+        local SELECTED_APP=""
+        
+        # Store the paths of all files within the directory 'WINAPPS_PATH'.
+        readarray -t ALL_FILES < <(find "$WINAPPS_PATH" -maxdepth 1 -type f)
 
-        # Store the filenames of files containing "${WINAPPS_PATH}/winapps" in an array.
+        # Ignore files that do not contain "${WINAPPS_PATH}/winapps".
         # Ignore files named "winapps" and "windows".
-        # Return an empty array if no such files exist.
-        readarray -t APP_LIST < <(find "$WINAPPS_PATH" -maxdepth 1 -type f ! -name "winapps" ! -name "windows" -exec grep -l "${WINAPPS_PATH}/winapps" {} + 2>/dev/null | xargs -I {} basename {})
+        for FILE in "${ALL_FILES[@]}"; do
+            if grep -q "${WINAPPS_PATH}/winapps" "$FILE" && [ "$(basename "$FILE")" != "windows" ] && [ "$(basename "$FILE")" != "winapps" ]; then
+                # Store the filename.
+                APP_LIST+=("$(basename "$FILE")")
+            fi
+        done
 
-        local selected_app=$(yad --list \
+        SELECTED_APP=$(yad --list \
         --title="WinApps Launcher" \
         --width=300 \
         --height=500 \
@@ -117,15 +128,15 @@ app_select() {
         --column="Application Name" \
         "${APP_LIST[@]}")
 
-        if [ -n "${selected_app}" ]; then
+        if [ -n "$SELECTED_APP" ]; then
             # Remove Trailing Bar
-            selected_app=$(echo $selected_app | cut -d"|" -f1)
+            SELECTED_APP=$(echo "$SELECTED_APP" | cut -d"|" -f1)
 
             # Print Feedback
-            echo -e "${DEBUG_TEXT}> LAUNCH '$selected_app'${RESET_TEXT}"
+            echo -e "${DEBUG_TEXT}> LAUNCH '${SELECTED_APP}'${RESET_TEXT}"
 
             # Run Selected Application
-            winapps $selected_app
+            winapps "$SELECTED_APP"
         fi
     fi
 }
@@ -146,13 +157,13 @@ export -f launch_windows
 # Check Valid Domain
 check_valid_domain() {
     # Check Virtual Machine State
-    VM_STATE=$(virsh domstate "${VM_NAME}" 2>&1 | xargs)
+    VM_STATE=$(virsh domstate "$VM_NAME" 2>&1 | xargs)
 
-    if grep -q "argument is empty" <<< "${VM_STATE}"; then
+    if grep -q "argument is empty" <<< "$VM_STATE"; then
         # Unspecified Domain
         show_error_message "ERROR: Windows VM <u>NOT SPECIFIED</u>.\nPlease ensure a virtual machine name is specified."
         exit 3
-    elif grep -q "failed to get domain" <<< "${VM_STATE}"; then
+    elif grep -q "failed to get domain" <<< "$VM_STATE"; then
         # Domain Not Found
         show_error_message "ERROR: Windows VM <u>NOT FOUND</u>.\nPlease ensure <i>'${VM_NAME}'</i> exists."
         exit 4
@@ -163,10 +174,12 @@ export -f check_valid_domain
 # Check Reachable
 check_reachable() {
     #VM_IP=$(virsh net-dhcp-leases default | grep "${VM_NAME}" | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}') # Unreliable since this does not always list VM
-    local VM_MAC=$(virsh domiflist "${VM_NAME}" | grep -Eo '([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})') # Virtual Machine MAC Address
-    local VM_IP=$(arp -n | grep "${VM_MAC}" | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}") # Virtual Machine IP Address
+    # shellcheck disable=SC2155 # Silence warning regarding declaring and assigning variables separately.
+    local VM_MAC=$(virsh domiflist "$VM_NAME" | grep -Eo '([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})') # Virtual Machine MAC Address
+    # shellcheck disable=SC2155 # Silence warning regarding declaring and assigning variables separately.
+    local VM_IP=$(arp -n | grep "$VM_MAC" | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}") # Virtual Machine IP Address
 
-    if [ -z "${VM_IP}" ]; then
+    if [ -z "$VM_IP" ]; then
         # Empty
         show_error_message "ERROR: Windows VM is <u>UNREACHABLE</u>.\nPlease ensure <i>'${VM_NAME}'</i> has an IP address."
         return 1
@@ -182,12 +195,12 @@ export -f check_reachable
 
 generate_menu() {
     # Check Virtual Machine State
-    VM_STATE=$(virsh domstate "${VM_NAME}" 2>&1 | xargs)
+    VM_STATE=$(virsh domstate "$VM_NAME" 2>&1 | xargs)
 
     # Print Feedback
     echo -e "${STATUS_TEXT}* VM STATE: ${VM_STATE^^}${RESET_TEXT}"
 
-    if [ "${VM_STATE}" = "running" ]; then
+    if [ "$VM_STATE" = "running" ]; then
         echo "menu:\
 ${MENU_APPLICATIONS}|\
 ${MENU_REDMOND}|\
@@ -200,7 +213,7 @@ ${MENU_RESET}|\
 ${MENU_KILL}|\
 ${MENU_REFRESH}|\
 ${MENU_QUIT}" >&3
-    elif [ "${VM_STATE}" = "paused" ]; then
+    elif [ "$VM_STATE" = "paused" ]; then
         echo "menu:\
 ${MENU_RESUME}|\
 ${MENU_HIBERNATE}|\
@@ -211,7 +224,7 @@ ${MENU_RESET}|\
 ${MENU_KILL}|\
 ${MENU_REFRESH}|\
 ${MENU_QUIT}" >&3
-    elif [ "${VM_STATE}" = "shut off" ]; then
+    elif [ "$VM_STATE" = "shut off" ]; then
         echo "menu:\
 ${MENU_POWERON}|\
 ${MENU_KILL}|\
@@ -227,11 +240,11 @@ function power_on_vm() {
     echo -e "${DEBUG_TEXT}> POWER ON VM${RESET_TEXT}"
 
     # Reopen PIPE
-    exec 3<> $PIPE
+    exec 3<> "$PIPE"
 
     # Issue Command
-    echo -e "${COMMAND_TEXT}% $(virsh start $VM_NAME | grep -v "^$")${RESET_TEXT}"
-    sleep $SLEEP_DURATION
+    echo -e "${COMMAND_TEXT}% $(virsh start "$VM_NAME" | grep -v "^$")${RESET_TEXT}"
+    sleep "$SLEEP_DURATION"
 
     # Refresh Menu
     generate_menu
@@ -244,15 +257,15 @@ function power_off_vm() {
     echo -e "${DEBUG_TEXT}> POWER OFF VM${RESET_TEXT}"
 
     # Reopen PIPE
-    exec 3<> $PIPE
+    exec 3<> "$PIPE"
 
-    if pgrep -x $RDP_COMMAND > /dev/null; then
+    if pgrep -x "$RDP_COMMAND" > /dev/null; then
         # FreeRDP Sessions Running
         show_error_message "ERROR: Powering Off Windows VM <u>FAILED</u>.\nPlease ensure all FreeRDP instance(s) are terminated."
     else
         # Issue Command
-        echo -e "${COMMAND_TEXT}% $(virsh shutdown $VM_NAME | grep -v "^$")${RESET_TEXT}"
-        sleep $SLEEP_DURATION
+        echo -e "${COMMAND_TEXT}% $(virsh shutdown "$VM_NAME" | grep -v "^$")${RESET_TEXT}"
+        sleep "$SLEEP_DURATION"
 
         # Refresh Menu
         generate_menu
@@ -266,15 +279,15 @@ function pause_vm() {
     echo -e "${DEBUG_TEXT}> PAUSE VM${RESET_TEXT}"
 
     # Reopen PIPE
-    exec 3<> $PIPE
+    exec 3<> "$PIPE"
 
-    if pgrep -x $RDP_COMMAND > /dev/null; then
+    if pgrep -x "$RDP_COMMAND" > /dev/null; then
         # FreeRDP Sessions Running
         show_error_message "ERROR: Pausing Windows VM <u>FAILED</u>.\nPlease ensure all FreeRDP instance(s) are terminated."
     else
         # Issue Command
-        echo -e "${COMMAND_TEXT}% $(virsh suspend $VM_NAME | grep -v "^$")${RESET_TEXT}"
-        sleep $SLEEP_DURATION
+        echo -e "${COMMAND_TEXT}% $(virsh suspend "$VM_NAME" | grep -v "^$")${RESET_TEXT}"
+        sleep "$SLEEP_DURATION"
 
         # Refresh Menu
         generate_menu
@@ -288,11 +301,11 @@ function resume_vm() {
     echo -e "${DEBUG_TEXT}> RESUME VM${RESET_TEXT}"
 
     # Reopen PIPE
-    exec 3<> $PIPE
+    exec 3<> "$PIPE"
 
     # Issue Command
-    echo -e "${COMMAND_TEXT}% $(virsh resume $VM_NAME | grep -v "^$")${RESET_TEXT}"
-    sleep $SLEEP_DURATION
+    echo -e "${COMMAND_TEXT}% $(virsh resume "$VM_NAME" | grep -v "^$")${RESET_TEXT}"
+    sleep "$SLEEP_DURATION"
 
     # Refresh Menu
     generate_menu
@@ -305,11 +318,11 @@ function reset_vm() {
     echo -e "${DEBUG_TEXT}> RESET VM${RESET_TEXT}"
 
     # Reopen PIPE
-    exec 3<> $PIPE
+    exec 3<> "$PIPE"
 
     # Issue Command
-    echo -e "${COMMAND_TEXT}% $(virsh reset $VM_NAME | grep -v "^$")${RESET_TEXT}"
-    sleep $SLEEP_DURATION
+    echo -e "${COMMAND_TEXT}% $(virsh reset "$VM_NAME" | grep -v "^$")${RESET_TEXT}"
+    sleep "$SLEEP_DURATION"
 
     # Refresh Menu
     generate_menu
@@ -322,15 +335,15 @@ function reboot_vm() {
     echo -e "${DEBUG_TEXT}> REBOOT VM${RESET_TEXT}"
 
     # Reopen PIPE
-    exec 3<> $PIPE
+    exec 3<> "$PIPE"
 
-    if pgrep -x $RDP_COMMAND > /dev/null; then
+    if pgrep -x "$RDP_COMMAND" > /dev/null; then
         # FreeRDP Sessions Running
         show_error_message "ERROR: Rebooting Windows VM <u>FAILED</u>.\nPlease ensure all FreeRDP instance(s) are terminated."
     else
         # Issue Command
-        echo -e "${COMMAND_TEXT}% $(virsh reboot $VM_NAME | grep -v "^$")${RESET_TEXT}"
-        sleep $SLEEP_DURATION
+        echo -e "${COMMAND_TEXT}% $(virsh reboot "$VM_NAME" | grep -v "^$")${RESET_TEXT}"
+        sleep "$SLEEP_DURATION"
 
         # Refresh Menu
         generate_menu
@@ -344,11 +357,11 @@ function force_power_off_vm() {
     echo -e "${DEBUG_TEXT}> FORCE POWER OFF VM${RESET_TEXT}"
 
     # Reopen PIPE
-    exec 3<> $PIPE
+    exec 3<> "$PIPE"
 
     # Issue Command
-    echo -e "${COMMAND_TEXT}% $(virsh destroy $VM_NAME --graceful | grep -v "^$")${RESET_TEXT}"
-    sleep $SLEEP_DURATION
+    echo -e "${COMMAND_TEXT}% $(virsh destroy "$VM_NAME" --graceful | grep -v "^$")${RESET_TEXT}"
+    sleep "$SLEEP_DURATION"
 
     # Refresh Menu
     generate_menu
@@ -361,14 +374,14 @@ function hibernate_vm() {
     echo -e "${DEBUG_TEXT}> HIBERNATE VM${RESET_TEXT}"
 
     # Reopen PIPE
-    exec 3<> $PIPE
-    if pgrep -x $RDP_COMMAND > /dev/null; then
+    exec 3<> "$PIPE"
+    if pgrep -x "$RDP_COMMAND" > /dev/null; then
         # FreeRDP Sessions Running
         show_error_message "ERROR: Hibernating Windows VM <u>FAILED</u>.\nPlease ensure all FreeRDP instance(s) are terminated."
     else
         # Issue Command
-        echo -e "${COMMAND_TEXT}% $(virsh managedsave $VM_NAME | grep -v "^$")${RESET_TEXT}"
-        sleep $SLEEP_DURATION
+        echo -e "${COMMAND_TEXT}% $(virsh managedsave "$VM_NAME" | grep -v "^$")${RESET_TEXT}"
+        sleep "$SLEEP_DURATION"
 
         # Refresh Menu
         generate_menu
@@ -382,7 +395,7 @@ function refresh_menu() {
     echo -e "${DEBUG_TEXT}> REFRESH MENU${RESET_TEXT}"
 
     # Reopen PIPE
-    exec 3<> $PIPE
+    exec 3<> "$PIPE"
 
     # Refresh Menu
     generate_menu
@@ -407,7 +420,7 @@ if ! command -v winapps &> /dev/null; then
     show_error_message "ERROR: 'winapps' <u>NOT FOUND</u>.\nPlease ensure 'winapps' is installed."
     exit 2
 else
-    WINAPPS_PATH=$(dirname $(which winapps))
+    WINAPPS_PATH=$(dirname "$(which winapps)")
 fi
 
 ### INITIALISATION ###
